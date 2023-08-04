a0391ef2cd31ecaed5b3015afc168281e247ace5
meghalakhwan <meghalakhwan9@gmail.com>
2023-08-04 22:07:17 +0530

    first commit

--- flags.js
+++ flags.js
@@ -31,3 +31,68 @@ function outArg() {
     document.getElementById("arg").innerHTML = c;
 }
 
+function ausFunc() 
+{
+    var c = "This is the flag of Australia";
+    document.getElementById("aus").innerHTML = c;
+}
+
+function outAus() {
+    var c = ""
+    document.getElementById("aus").innerHTML = c;
+}
+
+function canadaFunc() 
+{
+    var c = "This is the flag of Canada";
+    document.getElementById("canada").innerHTML = c;
+}
+
+function outCanada() {
+    var c = ""
+    document.getElementById("canada").innerHTML = c;
+}
+
+function franceFunc() 
+{
+    var c = "This is the flag of France";
+    document.getElementById("france").innerHTML = c;
+}
+
+function outFrance() {
+    var c = ""
+    document.getElementById("france").innerHTML = c;
+}
+
+function greeceFunc() 
+{
+    var c = "This is the flag of Greece";
+    document.getElementById("greece").innerHTML = c;
+}
+
+function outGreece() {
+    var c = ""
+    document.getElementById("greece").innerHTML = c;
+}
+
+function japanFunc() 
+{
+    var c = "This is the flag of Japan";
+    document.getElementById("japan").innerHTML = c;
+}
+
+function outJapan() {
+    var c = ""
+    document.getElementById("japan").innerHTML = c;
+}
+
+function russiaFunc() 
+{
+    var c = "This is the flag of Russia";
+    document.getElementById("russia").innerHTML = c;
+}
+
+function outRussia() {
+    var c = ""
+    document.getElementById("russia").innerHTML = c;
+}
