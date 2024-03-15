f819505fe1f0f54f0c718ca6c43f2e1902a119f1
meghalakhwan <meghalakhwan9@gmail.com>
2024-03-15 23:11:03 +0530

    Project Completed

--- flags.js
+++ flags.js
@@ -1,90 +1,89 @@
 function brazilFunc() {
-    var c = "This is the flag of Brazil";
-    document.getElementById("brazil").innerHTML = c;
+  var c = "This is the flag of Brazil";
+  document.getElementById("brazil").innerHTML = c;
 }
 
 function outBrazil() {
-    var c = ""
-    document.getElementById("brazil").innerHTML = c;
+  var c = "";
+  document.getElementById("brazil").innerHTML = c;
 }
 
 function indiaFunc() {
-    var c = "This is the flag of India";
-    document.getElementById("india").innerHTML = c;
+  var c = "This is the flag of India";
+  document.getElementById("india").innerHTML = c;
 }
 
 function outIndia() {
-    var c = ""
-    document.getElementById("india").innerHTML = c;
+  var c = "";
+  document.getElementById("india").innerHTML = c;
 }
 
 function argFunc() {
-    var c = "This is the flag of Argentina";
-    document.getElementById("arg").innerHTML = c;
+  var c = "This is the flag of Argentina";
+  document.getElementById("arg").innerHTML = c;
 }
 
 function outArg() {
-    var c = ""
-    document.getElementById("arg").innerHTML = c;
+  var c = "";
+  document.getElementById("arg").innerHTML = c;
 }
 
 function ausFunc() {
-    var c = "This is the flag of Australia";
-    document.getElementById("aus").innerHTML = c;
+  var c = "This is the flag of Australia";
+  document.getElementById("aus").innerHTML = c;
 }
 
 function outAus() {
-    var c = ""
-    document.getElementById("aus").innerHTML = c;
+  var c = "";
+  document.getElementById("aus").innerHTML = c;
 }
 
 function canadaFunc() {
-    var c = "This is the flag of Canada";
-    document.getElementById("canada").innerHTML = c;
+  var c = "This is the flag of Canada";
+  document.getElementById("canada").innerHTML = c;
 }
 
 function outCanada() {
-    var c = ""
-    document.getElementById("canada").innerHTML = c;
+  var c = "";
+  document.getElementById("canada").innerHTML = c;
 }
 
 function franceFunc() {
-    var c = "This is the flag of France";
-    document.getElementById("france").innerHTML = c;
+  var c = "This is the flag of France";
+  document.getElementById("france").innerHTML = c;
 }
 
 function outFrance() {
-    var c = ""
-    document.getElementById("france").innerHTML = c;
+  var c = "";
+  document.getElementById("france").innerHTML = c;
 }
 
 function greeceFunc() {
-    var c = "This is the flag of Greece";
-    document.getElementById("greece").innerHTML = c;
+  var c = "This is the flag of Greece";
+  document.getElementById("greece").innerHTML = c;
 }
 
-
 function outGreece() {
-    var c = ""
-    document.getElementById("greece").innerHTML = c;
+  var c = "";
+  document.getElementById("greece").innerHTML = c;
 }
 
 function japanFunc() {
-    var c = "This is the flag of Japan";
-    document.getElementById("japan").innerHTML = c;
+  var c = "This is the flag of Japan";
+  document.getElementById("japan").innerHTML = c;
 }
 
 function outJapan() {
-    var c = ""
-    document.getElementById("japan").innerHTML = c;
+  var c = "";
+  document.getElementById("japan").innerHTML = c;
 }
 
 function russiaFunc() {
-    var c = "This is the flag of Russia";
-    document.getElementById("russia").innerHTML = c;
+  var c = "This is the flag of Russia";
+  document.getElementById("russia").innerHTML = c;
 }
 
 function outRussia() {
-    var c = ""
-    document.getElementById("russia").innerHTML = c;
+  var c = "";
+  document.getElementById("russia").innerHTML = c;
 }
